6cf1d06d6cb8255f0aa64ee33b0836f1f9edd334
Neuber Oliveira <neuber.oliveira05@gmail.com>
2025-09-24 17:12:32 -0300

    Adiciona componente Footer à página Devocional
    Inclui o componente Footer para padronizar o layout da página. Remove importação não utilizada do inPeaceServiceVisual e ajusta o z-index das imagens para melhorar a exibição.

--- src/app/devocional/[id]/page.tsx
+++ src/app/devocional/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Main from '@/components/client/Structure/Main'
 import Header from '@/components/client/Structure/Header'
-import {inPeaceServiceDevocional, inPeaceServiceVisual} from '@/service/inpeace'
+import {inPeaceServiceDevocional} from '@/service/inpeace'
+import Footer from '@/components/client/Structure/Footer'
 
 export default async function DevocionalPage() {
   const devotionals = await inPeaceServiceDevocional()
@@ -12,13 +13,14 @@ export default async function DevocionalPage() {
       <div id="devotional-detail">
         <div className="relative w-full flex justify-center my-4 overflow-hidden rounded-t-lg">
           <img src={image} alt={devotional.descricao} className="object-cover w-full h-[320px] object-top absolute inset-0 opacity-70 blur-md scale-110 z-0"/>
-          <img src={image} alt={devotional.descricao} className="object-contain w-full h-[320px]"/>
+          <img src={image} alt={devotional.descricao} className="object-contain w-full h-[320px] z-10"/>
         </div>
 
         <h1 className="text-2xl font-semibold">{devotional.descricao}</h1>
         <hr className="my-2" />
         <div className="description mt-6" dangerouslySetInnerHTML={{__html: devotional.conteudo}} />
       </div>
+      <Footer />
     </Main>
   )
 }
