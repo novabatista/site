a9634190fce59d3809c449beb7370a7f6d12b688
Neuber Oliveira <neuber.oliveira05@gmail.com>
2026-01-21 10:07:07 -0300

    Condiciona renderização do componente GCEmbed
    Adicionada lógica de montagem para evitar a renderização do iframe antes do carregamento do componente. Ajustado também o cálculo de itens por página para otimizar a exibição do carrossel em diferentes breakpoints.

--- src/components/client/GCEmbed/GCEmbed.tsx
+++ src/components/client/GCEmbed/GCEmbed.tsx
@@ -1,13 +1,27 @@
 'use client'
 
 import useBreakpoint from '@/hook/useBreakpoint'
+import { useEffect, useState } from 'react'
 
 export default function GCEmbed({gcURL}) {
   const breakpoint = useBreakpoint()
+  const [mounted, setMounted] = useState(false)
+
+  const itemsPerPage = breakpoint.matcher({'base':1, 'sm': 1, 'md':2, 'xl':3})
+  const gcsSrc = `${gcURL}/embed?type=carousel&map=0&perpage=${itemsPerPage}`
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
   return (
     <iframe
       className="w-full h-[220px]"
-      src={`${gcURL}/embed?type=carousel&map=0&perpage=${breakpoint.matcher({'base':1, 'sm': 1, 'md':2, 'xl':3})}`}
+      src={gcsSrc}
       title="GCs" frameBorder="0"
       allow="clipboard-write; encrypted-media; web-share"
       referrerPolicy="strict-origin-when-cross-origin"
